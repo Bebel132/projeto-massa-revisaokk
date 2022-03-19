4abae6070cbfdd825f998708f93d49112d690bda
emanuel <emanuel2005batista@gmail.com>
2022-03-19 08:10:39 -0300

    evento de click para menu adicionado

--- script/script.js
+++ script/script.js
@@ -1,14 +1,18 @@
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    cssMode: true,
-    loop: true,
-    autoplay: {
-        delay: 1000,
-        disableOnInteraction: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    mousewheel: true,
-  });
+  spaceBetween: 30,
+  cssMode: true,
+  loop: true,
+  autoplay: {
+      delay: 1000,
+      disableOnInteraction: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  mousewheel: true,
+});
+
+let bars = document.querySelector('.fa-bars').addEventListener('click', () => {
+  let menu = document.querySelector('.cabecalho-menuParaCelular-hidden').classList.toggle('cabecalho-menuParaCelular');
+})
